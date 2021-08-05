02f9e949d37384cac438b5c86fc90821259785e5
kkldream <kk69347321@gmail.com>
2021-08-06 00:11:45 +0800

    ez upload test

--- esp32_mlx90640_uart_tx/src/main.cpp
+++ esp32_mlx90640_uart_tx/src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+	pinMode(LED_BUILTIN, OUTPUT);
+	Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	Serial.println("Hello");
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(1000);
 }
